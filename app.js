00dc775c0c6f52d570a8bf14d7a56ef0fea7f6c8
TarunKumarBisoi <32740407+TarunKumarBisoi@users.noreply.github.com>
2018-05-26 19:18:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,4 @@ cameraTrigger.onclick = function() {
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
 
-video.style.cssText = "-moz-transform: scale(-1, 1); \
--webkit-transform: scale(-1, 1); -o-transform: scale(-1, 1); \
-transform: scale(-1, 1); filter: FlipH;";
+
